13be61bddc43c20b10d795261a8f8fdcb041012d
Maruthek1dd <leonardomarusich96@gmail.com>
2024-07-19 19:24:34 -0300

    19/07-7

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // Cambia esto al puerto que tu entorno de despliegue espera
+    host: true, // Permite el acceso desde fuera del localhost
+  },
+});
