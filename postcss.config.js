ef92a87463dc05444ba57d0898f52f81a164280f
Maruthek1dd <leonardomarusich96@gmail.com>
2024-07-19 22:58:03 -0300

    Normalize line endings4

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+import postcssImport from "postcss-import";
+import postcssNested from "postcss-nested";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
+
+export default {
+  plugins: [postcssImport(), postcssNested(), autoprefixer(), tailwindcss()],
+};
