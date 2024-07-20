5af7ce6f91c87698c3538b79027d2deb5e898763
Maruthek1dd <leonardomarusich96@gmail.com>
2024-07-19 21:40:43 -0300

    19/07-14

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+    require("tailwindcss"),
+  ],
+};
