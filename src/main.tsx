fd681eb9bfbacdb70846b69353e018f0a5816feb
Maruthek1dd <leonardomarusich96@gmail.com>
2024-07-19 22:08:22 -0300

    19/07-17

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,17 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Education from "./components/Education";
 import Technologies from "./components/Technologies";
+
 import { Proyect } from "./components/Proyect";
 import { About } from "./components/About";
 import { Skills } from "./components/Skills";
+// Verifica que el elemento con id="root" exista en tu archivo index.html
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-800 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full">
+      {/* <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      </div>
+      </div> */}
       <div className="container mx-auto px-8 flex flex-col min-h-screen">
         <Navbar />
         <Hero />
