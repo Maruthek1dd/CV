b80a5cd1afc73c335e34836589b1d9b2c717d6b5
Maruthek1dd <leonardomarusich96@gmail.com>
2024-07-22 17:08:02 -0300

    Normalize line endings5

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
+// .eslintrc.cjs
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow jsx syntax in js files (for next.js project)
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
   },
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+};
